e956a16c4ac8a03799bafd995572686430ec6496
cloud_nzk <526416486@qq.com>
2022-06-13 18:17:04 +0800

    翻译句子包含单引号问题

--- server/index.js
+++ server/index.js
@@ -6,10 +6,11 @@ const exec = util.promisify(require('child_process').exec);
 const app = express()
 
 app.get('/translation/:text',async(req,res) => {
-    command = "echo "+ req.params.text + " | python3 ./utils/interactive.py ../../model/data/dataset/data-bin \
+    let english_sentence = req.params.text.replace("'","\'")
+    command = `echo -e ` + `"${english_sentence}"` + `| python3 ./utils/interactive.py ../../model/data/dataset/data-bin \
     --path ../../model/model/checkpoint_best.pt \
     --buffer-size 2000 --batch-size 256 \
-    --remove-bpe --beam 5 --max-len-a 1.2 --max-len-b 10 | grep ^H  | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g'"
+    --remove-bpe --beam 5 --max-len-a 1.2 --max-len-b 10 | grep ^H  | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g'`
     
     const {stdout} = await exec(command);
     console.log(stdout);
